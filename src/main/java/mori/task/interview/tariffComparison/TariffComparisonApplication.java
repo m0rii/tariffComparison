6dd3a17e4792413430d0d90672f31741cb479dfd
m0rii <morteza.nabhan@gmail.com>
2022-11-03 15:39:14 +0100

    Initial commit

---
+++ src/main/java/mori/task/interview/tariffComparison/TariffComparisonApplication.java
@@ -0,0 +1,13 @@
+package mori.task.interview.tariffComparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffComparisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mori/task/interview/tariffComparison/TariffComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package mori.task.interview.tariffComparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
